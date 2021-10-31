4f71c093db9fa4bff32e417d02a280157e1c09d4
0p3r4t0r <robert83sutherland@gmail.com>
2021-10-31 12:00:42 +0900

    Render a basic typemason keyboard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
 import React from "react";
-import { KeyCap, KeyLegend, KeyRow } from "components/organisms/Keyboard";
+import { Keyboard, KeyMap } from "components/organisms/Keyboard";
+
+const layoutUS: KeyMap = [
+  [
+    { key: "1", shift: "!", code: 1 },
+    { key: "2", shift: "@", code: 1 },
+    { key: "3", shift: "#", code: 1 },
+    { key: "4", shift: "$", code: 1 },
+    { key: "5", shift: "%", code: 1 },
+    { key: "6", shift: "^", code: 1 },
+    { key: "7", shift: "&", code: 1 },
+    { key: "8", shift: "*", code: 1 },
+    { key: "9", shift: "(", code: 1 },
+    { key: "0", shift: ")", code: 1 },
+    { key: "-", shift: "_", code: 1 },
+  ],
+  [
+    { key: "Q", code: 1 },
+    { key: "W", code: 1 },
+    { key: "E", code: 1 },
+    { key: "R", code: 1 },
+    { key: "T", code: 1 },
+    { key: "Y", code: 1 },
+    { key: "U", code: 1 },
+    { key: "I", code: 1 },
+    { key: "O", code: 1 },
+    { key: "P", code: 1 },
+  ],
+  [
+    { key: "A", code: 1 },
+    { key: "S", code: 1 },
+    { key: "D", code: 1 },
+    { key: "F", code: 1 },
+    { key: "G", code: 1 },
+    { key: "H", code: 1 },
+    { key: "J", code: 1 },
+    { key: "K", code: 1 },
+    { key: "L", code: 1 },
+    { key: ";", shift: ":", code: 1 },
+    { key: "'", shift: "\"", code: 2 },
+  ],
+  [
+    { key: "Z", code: 1 },
+    { key: "X", code: 1 },
+    { key: "C", code: 1 },
+    { key: "V", code: 1 },
+    { key: "B", code: 1 },
+    { key: "N", code: 1 },
+    { key: "M", code: 1 },
+    { key: ",", shift: "<", code: 1 },
+    { key: ".", shift: ">", code: 1 },
+    { key: "/", shift: "/", code: 1 },
+  ],
+];
+
 function App() {
-  return (
-    <KeyRow>
-      <KeyCap>
-        <KeyLegend>A</KeyLegend>
-      </KeyCap>
-      <KeyCap>
-        <KeyLegend>T</KeyLegend>
-      </KeyCap>
-      <KeyCap>
-        <KeyLegend>O</KeyLegend>
-      </KeyCap>
-      <KeyCap>
-        <KeyLegend>M</KeyLegend>
-      </KeyCap>
-      <KeyCap>
-        <KeyLegend>S</KeyLegend>
-      </KeyCap>
-    </KeyRow>
-  );
+  return <Keyboard keymap={layoutUS} />;
 }
 
 export default App;
--- src/components/organisms/Keyboard/index.tsx
+++ src/components/organisms/Keyboard/index.tsx
@@ -1,17 +1,72 @@
 import React from "react";
 import "static/keymason/keymason/style.css";
 
-export const KeyLegend = (props: { children: React.ReactNode }) => {
-  const KeyLegendTag = "k-legend" as keyof JSX.IntrinsicElements;
-  return <KeyLegendTag className="small">{props.children}</KeyLegendTag>;
+// =============================================================================
+// Key Mason React Components
+// =============================================================================
+
+type KeyMasonProps = {
+  children: React.ReactNode;
+  class?: string;
+  style?: { [key: string]: string | number }; // TODO: Add types for custom styles
 };
 
-export const KeyCap = (props: { children: React.ReactNode }) => {
-  const KeyCapTag = "k-cap" as keyof JSX.IntrinsicElements;
-  return <KeyCapTag className="mod"> {props.children} </KeyCapTag>;
+const KeyMasonComponent = function (props: KeyMasonProps & { tag: string }) {
+  const { tag, ...other } = props;
+  const Tag = props.tag as keyof JSX.IntrinsicElements;
+  return <Tag {...other}>{props.children}</Tag>;
 };
 
-export const KeyRow = (props: { children: React.ReactNode }) => {
-  const KeyRowTag = "k-row" as keyof JSX.IntrinsicElements;
-  return <KeyRowTag className="mod"> {props.children} </KeyRowTag>;
+export const KeyLegend = (props: KeyMasonProps) => (
+  <KeyMasonComponent tag="k-legend" {...props}>
+    {props.children}
+  </KeyMasonComponent>
+);
+
+export const KeyCap = (props: KeyMasonProps) => (
+  <KeyMasonComponent tag="k-cap" {...props}>
+    {props.children}
+  </KeyMasonComponent>
+);
+
+export const KeyRow = (props: KeyMasonProps) => (
+  <KeyMasonComponent tag="k-row" {...props}>
+    {props.children}
+  </KeyMasonComponent>
+);
+
+// =============================================================================
+// KeyBoard
+// =============================================================================
+
+export type KeyMapKey = {
+  code: number;
+  key: string;
+  shift?: string;
 };
+
+export type KeyMapRow = KeyMapKey[];
+
+export type KeyMap = KeyMapRow[];
+
+export const Keyboard = (props: { keymap: KeyMap }) => (
+  <div id="keyboard" className="selectable">
+    {props.keymap.map((row, index) => (
+      <KeyRow style={index % 2 === 1 ? { "--x": "0.5" } : undefined}>
+        {row.map((key) => (
+          // Assign active class to select
+          <KeyCap> 
+            {Boolean(key.shift) ? (
+              <>
+                <KeyLegend class="medium top left">{key.shift}</KeyLegend>
+                <KeyLegend class="medium bottom left">{key.key}</KeyLegend>
+              </>
+            ) : (
+              <KeyLegend>{key.key}</KeyLegend>
+            )}
+          </KeyCap>
+        ))}
+      </KeyRow>
+    ))}
+  </div>
+);
